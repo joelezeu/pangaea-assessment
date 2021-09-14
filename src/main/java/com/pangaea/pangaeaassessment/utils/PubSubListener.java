c646add652a0e01c9266a640a88d796b8da98d04
Joel Eze <joel.eze@rensource.energy>
2021-09-14 19:16:54 +0100

    Added more comments o the Library used

--- src/main/java/com/pangaea/pangaeaassessment/utils/PubSubListener.java
+++ src/main/java/com/pangaea/pangaeaassessment/utils/PubSubListener.java
@@ -10,6 +10,10 @@ public class PubSubListener {
     static ConcurrentMap<String, Object> concurrentMap = null;
 
     //A  singleton to statically get items from a Concurrent Map
+    //ConcurrentMap gives the ability for the Subscriber to access the data from the publisher Concurrently.
+    //Decision to use ConcurrentMap was to make the assessment as simple as possible
+    //Only disadvantage is that it can only work in the same process, meaning, both Publisher and the Subscriber will
+    //reside in this process.
     public static ConcurrentMap getConcurrentMap() {
         if (concurrentMap == null || concurrentMap.isEmpty()) {
             concurrentMap = new ConcurrentHashMap();
