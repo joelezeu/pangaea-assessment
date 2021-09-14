dd1a5e76fd3fe47d9e581de5766e37dfaefb3cbf
Joel Eze <joel.eze@rensource.energy>
2021-09-14 08:28:06 +0100

    Added a publish endpoint with a pathvariable of topic

--- src/main/java/com/pangaea/pangaeaassessment/controllers/Publisher.java
+++ src/main/java/com/pangaea/pangaeaassessment/controllers/Publisher.java
@@ -2,6 +2,9 @@ package com.pangaea.pangaeaassessment.controllers;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,4 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/publish")
 @RequiredArgsConstructor
 public class Publisher {
+    @PostMapping("/{topic}")
+    public ResponseEntity<?> publish(@PathVariable String topic) {
+        return null;
+    }
 }
