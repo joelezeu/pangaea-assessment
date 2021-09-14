6e61d16b90db07556fa7109ce2d2bb65a669d331
Joel Eze <joel.eze@rensource.energy>
2021-09-14 08:24:20 +0100

    Added the first skeletal controller for the Publisher and One Subscriber

--- src/main/java/com/pangaea/pangaeaassessment/controllers/Publisher.java
+++ src/main/java/com/pangaea/pangaeaassessment/controllers/Publisher.java
@@ -1,4 +1,13 @@
 package com.pangaea.pangaeaassessment.controllers;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/publish")
+@RequiredArgsConstructor
 public class Publisher {
 }
--- src/main/java/com/pangaea/pangaeaassessment/controllers/Subscriber1.java
+++ src/main/java/com/pangaea/pangaeaassessment/controllers/Subscriber1.java
@@ -1,7 +1,13 @@
 package com.pangaea.pangaeaassessment.controllers;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
+@RequestMapping(value = "/subscribe")
+@RequiredArgsConstructor
 public class Subscriber1 {
 }
